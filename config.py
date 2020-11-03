0ec4ad083ff8efbe38271a536cab44e7bcb8fd46
Jeff-Mwai <jeffmwai3@gmail.com>
2020-11-03 21:00:43 +0300

    Added SQLALCHEMY_URI

--- config.py
+++ config.py
@@ -1,23 +1,23 @@
 import os
 
 
-# class Config:
-#     SECRET_KEY = os.environ.get('SECRET_KEY')
-#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:KingJeffa00*@localhost/blog'
-#     UPLOADED_PHOTOS_DEST ='app/static/photos'
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:KingJeffa00*@localhost/blog'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
   
     
 
-# class ProdConfig(Config):
-#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:KingJeffa00*@localhost/blog'
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:KingJeffa00*@localhost/blog'
 
-# class TestConfig(Config):
-#     pass
+class TestConfig(Config):
+    pass
 
-# class DevConfig(Config):
-#     DEBUG = True
+class DevConfig(Config):
+    DEBUG = True
 
-# config_options = {
-# 'development':DevConfig,
-# 'production':ProdConfig
-# }
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
