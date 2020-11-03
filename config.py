f6642cb1f90905ab375b38d7868c3e3663dc9976
Jeff-Mwai <jeffmwai3@gmail.com>
2020-11-03 22:27:51 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ import os
 
 
 class Config:
-    QUOTE_API_BASE_URL ='http://quotes.stormconsultancy.co.uk/random.json'
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:KingJeffa00*@localhost/blog'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
